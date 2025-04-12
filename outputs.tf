5693a696b1215ff42ac3b32b737f3c08ee89c7d9
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2025-04-12 13:47:11 +0545

    Add outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,19 @@ output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.prod-k8s.name
   description = "The name of the AKS cluster"
 }
+
+output "storage_account_name" {
+  value       = azurerm_storage_account.storageaccount.name
+  description = "The name of the Azure Storage account"
+}
+
+output "storage_container_name" {
+  value       = azurerm_storage_container.storagecontainer.name
+  description = "The name of the Azure Storage container"
+}
+
+output "storage_account_key" {
+  value       = azurerm_storage_account.storageaccount.primary_access_key
+  description = "The primary access key of the Azure Storage account"
+  sensitive   = true
+}
